e207d0b5eaef0d21181f4459b40b4fcfa162416e
Jason Graffius <jasongraffius@gmail.com>
2016-09-23 19:29:31 -0400

    Expanded determine_paths doc, added doctest examples

--- dirge.py
+++ dirge.py
@@ -4,13 +4,59 @@
 from __future__ import absolute_import, division, unicode_literals, print_function
 
 
-def determine_paths(read):
-    """Determines the list of paths to create from a string or file containing
+def determine_paths(read, parent=None):
+    """Determines the list of paths to create from a directory specification
 
     :param read: text to parse for generation rules
     :type read: str, file
     :return: list of paths that should be generated
     :rtype: list
+
+    :Example with string:
+
+    >>> text = '''
+    ...
+    ... path
+    ...   |
+    ...   +-along
+    ...   |  |
+    ...   |  +-the
+    ...   |  |
+    ...   |  +-way
+    ...   |  |
+    ...   |  `-down
+    ...   |
+    ...   `-for
+    ...      |
+    ...      `-dirs
+    ...
+    ... '''
+    >>> paths = determine_paths(text)
+    >>> paths == [
+    ...     'path',
+    ...     'path/along',
+    ...     'path/along/down',
+    ...     'path/along/the',
+    ...     'path/along/way',
+    ...     'path/for',
+    ...     'path/for/dirs',
+    ... ]
+    True
+
+    :Example with absolute paths:
+
+    >>> text = '''
+    ... dir_one
+    ...   +-dir_two
+    ...   `-dir_three
+    ... '''
+    >>> paths = determine_paths(text, "/parent/directory/path")
+    >>> paths == [
+    ...     '/parent/directory/path/dir_one',
+    ...     '/parent/directory/path/dir_one/dir_two',
+    ...     '/parent/directory/path/dir_one/dir_three',
+    ... ]
+    True
     """
     pass
 
