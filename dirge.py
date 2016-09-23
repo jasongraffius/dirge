d8f5bef83ab6ac243b461c49b2b1453f970c7bd1
Jason Graffius <jmgraffi@ncsu.edu>
2016-09-23 19:13:15 -0400

    Docstring and stub for determine_paths

--- dirge.py
+++ dirge.py
@@ -4,6 +4,17 @@
 from __future__ import absolute_import, division, unicode_literals, print_function
 
 
+def determine_paths(read):
+    """Determines the list of paths to create from a string or file containing
+
+    :param read: text to parse for generation rules
+    :type read: str, file
+    :return: list of paths that should be generated
+    :rtype: list
+    """
+    pass
+
+
 def main():
     pass
 
