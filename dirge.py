c56f7e38c0e799b16d3021c861d188c12c7da487
Jason Graffius <jmgraffi@ncsu.edu>
2016-09-23 19:04:34 -0400

    Preparing for 2/3 compatiblity

--- dirge.py
+++ dirge.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-"""Dirge: Directory generator"""
+"""Dirge - Directory generator"""
+
+from __future__ import absolute_import, division, unicode_literals, print_function
 
 
 def main():
