062c807b047f28a2d1612b6914cf3460862ce42e
Jason Graffius <jasongraffius@gmail.com>
2016-09-24 17:07:29 -0400

    Moved _is_str to util (now named is_str)

--- dirge.py
+++ dirge.py
@@ -4,40 +4,6 @@
 from __future__ import absolute_import, division, unicode_literals, print_function
 
 
-def _is_str(obj):
-    """Determine if an object is string-like or a string.
-
-    Intended to be cross compatible with python 2 and 3
-
-    :param obj: Object to test if it is a string or string-like object
-    :type obj: str, ...
-    :return: True if the object is a string or inherits from the base string class for a given version
-    :rtype: bool
-
-    :Example:
-
-    >>> _is_str([1, 2, 3])
-    False
-    >>> _is_str('this is a string')
-    True
-    >>> _is_str(5)
-    False
-    >>> _is_str({'a', 'b', 'c'})
-    False
-    >>> _is_str('')
-    True
-
-    """
-
-    try:
-        # Attempt to try python-2 style
-        # noinspection PyCompatibility
-        return isinstance(obj, basestring)
-    except NameError:
-        # If basestring failed, try assuming python-3
-        return isinstance(obj, str)
-
-
 def parse_line(line):
     """Parses a single line of a directory specification.
 
---
+++ util/compat.py
@@ -0,0 +1,37 @@
+"""Provide helper functions for compatibility between python 2 and python 3"""
+
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+
+def is_str(obj):
+    """Determine if an object is string-like or a string.
+
+    Intended to be cross compatible with python 2 and 3
+
+    :param obj: Object to test if it is a string or string-like object
+    :type obj: str, ...
+    :return: True if the object is a string or inherits from the base string class for a given version
+    :rtype: bool
+
+    :Example:
+
+    >>> is_str([1, 2, 3])
+    False
+    >>> is_str('this is a string')
+    True
+    >>> is_str(5)
+    False
+    >>> is_str({'a', 'b', 'c'})
+    False
+    >>> is_str('')
+    True
+
+    """
+
+    try:
+        # Attempt to try python-2 style
+        # noinspection PyCompatibility
+        return isinstance(obj, basestring)
+    except NameError:
+        # If basestring failed, try assuming python-3
+        return isinstance(obj, str)
