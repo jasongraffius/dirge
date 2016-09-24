2ba6155d577ef20f77d60781d8d47791ce4aa78b
Jason Graffius <jmgraffi@ncsu.edu>
2016-09-24 14:06:02 -0400

    Created stub for parse_line

--- dirge.py
+++ dirge.py
@@ -4,6 +4,30 @@
 from __future__ import absolute_import, division, unicode_literals, print_function
 
 
+def parse_line(line):
+    """Parses a single line of a directory specification.
+
+    This function should search a line for a directory name, and then return that name and its indent length
+
+    :param line: Line to parse
+    :type line: str
+    :return: A tuple containing the directory name and the level of the directory
+    :rtype: tuple
+
+    :Example:
+
+    >>> parse_line(' | | +-directory')
+    ('directory', 6)
+    >>> parse_line('-root')
+    ('root', 0)
+    >>> parse_line('        deep-folder')
+    ('deep-folder', 8)
+    >>> parse_line('-----> folder')
+    ('folder', 7)
+    """
+    pass
+
+
 def determine_paths(read, parent=None):
     """Determines the list of paths to create from a directory specification
 
