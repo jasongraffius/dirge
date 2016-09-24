2b65b092ae88fa105ac52105d908546a21b48cca
Jason Graffius <jmgraffi@ncsu.edu>
2016-09-24 16:51:18 -0400

    Created util subpackage

---
+++ util/__init__.py
@@ -0,0 +1,9 @@
+"""util - utility functions, classes, decorators.
+
+This subpackage provides any helper functions and objects needed to abstract
+away messy or complex constructs, to provide utility definitions to prevent
+code duplication and reduce coupling, or to be more 'self-documenting'.
+
+"""
+
+# This __init__file intentionally left blank
